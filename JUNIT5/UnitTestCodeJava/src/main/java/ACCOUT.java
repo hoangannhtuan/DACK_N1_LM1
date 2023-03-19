2dc904266e9a627bf6ab08feef3349011d6d7bfe
hoangannhtuan <127833320+hoangannhtuan@users.noreply.github.com>
2023-03-19 12:31:21 +0700

    TUANANH Upload

--- JUNIT5/UnitTestCodeJava/src/main/java/ACCOUT.java
+++
@@ -1,9 +0,0 @@
-public class ACCOUT {
-    String tendangnhap, password;
-
-    public ACCOUT(String tendangnhap, String password) {
-        this.tendangnhap = tendangnhap;
-        this.password = password;
-    }
-
-}
